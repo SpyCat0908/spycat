8cec90f90a9695b733dd044e3a632ef8909578e7
GIULIA MONTE BLANCO HAPPKE <giulia_happke@estudante.sesisenai.org.br>
2023-11-14 22:02:25 -0300

    ajuste deleteUserTest

--- src/test/java/Araiguma/SpyCat/LostPet/LostPetServiceTest.java
+++ src/test/java/Araiguma/SpyCat/LostPet/LostPetServiceTest.java
@@ -1,5 +1,17 @@
 package Araiguma.SpyCat.LostPet;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import Araiguma.SpyCat.Services.LostPetService;
+
+@SpringBootTest
 public class LostPetServiceTest {
+    @InjectMocks
+    private LostPetService service;
+
+    @Mock
+    
     
 }
